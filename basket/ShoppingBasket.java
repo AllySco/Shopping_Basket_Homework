91a363c5d15683b9afa4b6aee63a3013f8811080
Ally Gerrie <alasdairwgerrie@gmail.com>
2017-06-09 16:11:35 +0100

    adding shopping basket class

---
+++ basket/ShoppingBasket.java
@@ -0,0 +1,21 @@
+package basket;
+import java.util.*;
+
+public class ShoppingBasket {
+   private int accountNumber;
+   private ArrayList<Productible> products;
+   private double totalCost;
+
+   public ShoppingBasket() {
+    this.accountNumber = accountNumber;
+    this.basket = basket;
+    this.totalCost = totalCost;
+   }
+
+
+
+
+
+
+
+}
---
+++ specs/ShoppingBasketTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import basket.*;
+
+public class ShoppingBasketTest {
+
+  @Before
+  public void before () {
+    ShoppingBasket shoppingBasket = new ShoppingBasket();
+  }
+
+}
